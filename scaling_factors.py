2090920fb11fc055bfaff39b27c0f341cc91e757
floracharbo <flora.charbonnier@live.fr>
2022-09-23 10:55:17 +0100

    assess performance alternative factor residual distributions

--- scaling_factors.py
+++ scaling_factors.py
@@ -1,7 +1,6 @@
 """Get scaling factor transitions characteristics."""
 
 import pickle
-from pathlib import Path
 from typing import List, Optional, Tuple
 
 import matplotlib.pyplot as plt
@@ -15,9 +14,8 @@ from utils import initialise_dict
 def _get_correlation(
         f_prevs_all: List[float],
         f_nexts_all: List[float],
-        save_path: Path,
+        prm: dict,
         data_type: str,
-        plots,
         label: Optional[str] = None):
 
     factors = {}
@@ -46,8 +44,10 @@ def _get_correlation(
         assert f_prev >= 0, f"{label} i {i} f_prev = {f_prev}"
         assert f_next >= 0, f"{label} i {i} f_next = {f_next}"
 
+    factors_path = prm["save_path"] / "factors"
     # plot f_next vs f_prev
-    if plots:
+    if prm["plots"]:
+
         for stylised in [True, False]:
             fig = plt.figure()
             plt.plot(f_prevs, f_nexts, "o", label="data", alpha=0.1)
@@ -74,10 +74,10 @@ def _get_correlation(
                 )
                 plt.legend()
 
-            fig.savefig(save_path / "factors" / title.replace(" ", "_"))
+            fig.savefig(factors_path / title.replace(" ", "_"))
             plt.close("all")
 
-    np.save(save_path / "factors" / f"corr_{data_type}_{label}", factors["corr"])
+    np.save(factors_path / f"corr_{data_type}_{label}", factors["corr"])
     factors["f_prevs"] = f_prevs
     factors["f_nexts"] = f_nexts
 
@@ -126,16 +126,16 @@ def _transition_intervals(
         f_prevs: List[float],
         f_nexts: List[float],
         transition: str,
-        n_intervals: int,
-        save_path: Path,
-        plots: bool
+        prm: int,
 ) -> Tuple[np.ndarray, List[float], np.ndarray, List[float]]:
     xs = np.linspace(min(min(f_prevs), min(f_nexts)),
                      max(max(f_prevs), max(f_nexts)),
-                     n_intervals + 1)
-    mid_xs = [np.mean(xs[i: i + 2]) for i in range(n_intervals)]
+                     prm["n_intervals"] + 1)
+    mid_xs = [np.mean(xs[i: i + 2]) for i in range(prm["n_intervals"])]
 
-    p_pos, p_zero2pos = _count_transitions(f_prevs, f_nexts, n_intervals, xs)
+    p_pos, p_zero2pos = _count_transitions(
+        f_prevs, f_nexts, prm["n_intervals"], xs
+    )
 
     labels_prob = [
         "EV non zero factor transition probabilities",
@@ -147,19 +147,21 @@ def _transition_intervals(
         trans_prob[(trans_prob == 0) | (np.isnan(trans_prob))] = 1e-5
         assert (trans_prob >= 0).all(), "error not (trans_prob >= 0).all()"
 
-        if plots:
+        if prm["plots"]:
             fig, ax = plt.subplots()
             ax.imshow(trans_prob, norm=LogNorm())
             # fig.colorbar(im, ax = ax)
             tick_labels = [0] + [xs[int(i - 1)] for i in ax.get_xticks()][1:]
-            tick_labels_format = ["{:.1f}".format(label) for label in tick_labels]
-            title = f"{label_prob}, {transition} n_intervals {n_intervals}"
+            tick_labels_format \
+                = ["{:.1f}".format(label) for label in tick_labels]
+            title \
+                = f"{label_prob} {transition} n_intervals {prm['n_intervals']}"
             plt.title(title)
             ax.set_xticklabels(tick_labels_format)
             ax.set_yticklabels(tick_labels_format)
             ax.set_xlabel("f(t + 1)")
             ax.set_ylabel("f(t)")
-            fig.savefig(save_path / "factors" / title.replace(" ", "_"))
+            fig.savefig(prm["save_path"] / "factors" / title.replace(" ", "_"))
 
     return p_pos, p_zero2pos, xs, mid_xs
 
@@ -173,7 +175,21 @@ def _print_error_factors(ex, label, f_prevs, f_nexts):
             print(f"np.isnan(f) = {np.isnan(factor)}")
 
 
-def _fit_gamma(f_prevs, f_nexts, save_path, data_type, plots, label=None):
+def _compare_factor_error_distributions(prm, errors, save_label):
+    sum_log_likelihood = {}
+    for label, distr in prm["candidate_factor_distributions"].items():
+        prms = distr.fit(errors)
+        sum_log_likelihood[label] = 0
+        for error in errors:
+            sum_log_likelihood[label] += np.log(distr.pdf(error, *prms))
+    with open(
+            prm["save_path"] / "factors" / f"sum_log_likelihood_{save_label}.pickle",
+            "wb"
+    ) as file:
+        pickle.dump(sum_log_likelihood, file)
+
+
+def _fit_gamma(f_prevs, f_nexts, prm, data_type, label=None):
     assert sum(1 for f_prev in f_prevs if f_prev is None) == 0,\
         "None f_prevs"
 
@@ -184,7 +200,7 @@ def _fit_gamma(f_prevs, f_nexts, save_path, data_type, plots, label=None):
     gamma_prms = gamma.fit(errors)
 
     # plot
-    if plots:
+    if prm["plots"]:
         fig = plt.figure()
         plt.hist(errors, density=1, alpha=0.5, label="data")
         x = np.linspace(
@@ -200,14 +216,14 @@ def _fit_gamma(f_prevs, f_nexts, save_path, data_type, plots, label=None):
             f"perfect correlation {data_type} {label}"
         )
         plt.title(title)
-        fig.savefig(save_path / "factors" / title.replace(" ", "_"))
+        fig.savefig(prm["save_path"] / "factors" / title.replace(" ", "_"))
         plt.close("all")
 
     mean_residual = gamma.stats(*gamma_prms, moments="m")
-
-    sum_log_likelihood = 0
-    for error in errors:
-        sum_log_likelihood += np.log(gamma.pdf(error, *gamma_prms))
+    if prm["test_factor_distr"]:
+        _compare_factor_error_distributions(
+            prm, errors, f"{data_type}_{label}"
+        )
 
     return mean_residual, gamma_prms
 
@@ -216,7 +232,7 @@ def _enough_data(banks_, weekday_type):
     return sum(banks_[day_type] is None for day_type in weekday_type) == 0
 
 
-def _ev_transitions(prm, factors, save_path):
+def _ev_transitions(prm, factors):
     p_pos, p_zero2pos, xs, mid_xs = [{} for _ in range(4)]
 
     for transition in prm["day_trans"]:
@@ -225,19 +241,21 @@ def _ev_transitions(prm, factors, save_path):
         # EV transitions
         [p_pos[transition], p_zero2pos[transition],
          xs[transition], mid_xs[transition]] = _transition_intervals(
-            f_prevs, f_nexts, transition, prm["n_intervals"], save_path, prm["plots"])
+            f_prevs, f_nexts, transition, prm
+        )
     dictionaries = p_pos, p_zero2pos, xs, mid_xs
     labels = ["p_pos", "p_zero2pos", "xs", "mid_xs"]
     for dictionary, label in zip(dictionaries, labels):
-        with open(save_path / "factors" / f"EV_{label}.pickle", "wb") as file:
+        with open(
+                prm["save_path"] / "factors" / f"EV_{label}.pickle", "wb"
+        ) as file:
             pickle.dump(dictionary, file)
 
 
 def _scaling_factors_behaviour_types(
         prm: dict,
         banks: dict,
-        save_path: Path
-) -> Tuple[dict, dict, dict]:
+) -> Tuple[dict, dict, dict, dict]:
     mean_residual, gamma_prms = [
         initialise_dict(prm["CLNR_types"], "empty_dict") for _ in range(2)
     ]
@@ -253,17 +271,22 @@ def _scaling_factors_behaviour_types(
                 = [banks[data_type][transition][f"f{i_f}"]
                    for i_f in range(2)]
             if len(f_prevs_all) == 0:
-                print(f"{data_type} transition {transition} len f_prevs_all == 0")
+                print(
+                    f"{data_type} transition {transition} "
+                    f"len f_prevs_all == 0"
+                )
             factors[data_type][transition] = _get_correlation(
-                f_prevs_all, f_nexts_all, save_path, data_type, prm["plots"], transition)
+                f_prevs_all, f_nexts_all, prm,
+                data_type, transition
+            )
             n_transitions[data_type] += len(f_prevs_all)
 
     if "dem" in prm["data_types"]:
         for transition in prm["day_trans"]:
             # Household demand
             if factors["dem"][transition] is None:
-                mean_residual["dem"][transition], gamma_prms["dem"][transition] \
-                    = None, None
+                mean_residual["dem"][transition] = None
+                gamma_prms["dem"][transition] = None
                 print(f"factors['dem'][{transition}] is None")
                 continue
 
@@ -274,65 +297,70 @@ def _scaling_factors_behaviour_types(
             assert sum(1 for f_prev in f_prevs if f_prev == 0) == 0, \
                 "need to account for Nones for fit gamma"
             mean_residual["dem"][transition], gamma_prms["dem"][transition] \
-                = _fit_gamma(f_prevs, f_nexts, save_path, "dem", prm["plots"], transition)
+                = _fit_gamma(
+                f_prevs, f_nexts, prm, "dem", transition
+            )
             if mean_residual["dem"][transition] is None:
-                print(f"mean_residual['dem'][{transition}] = {mean_residual['dem'][transition]}")
+                print(f"mean_residual['dem'][{transition}] "
+                      f"= {mean_residual['dem'][transition]}")
                 print(f"np.shape(f_prevs) = {np.shape(f_prevs)}")
                 print(f"np.shape(f_nexts) = {np.shape(f_nexts)}")
 
     if "EV" in prm["data_types"] \
             and _enough_data(banks["EV"], prm["weekday_type"]):
-        _ev_transitions(prm, factors, save_path)
+        _ev_transitions(prm, factors)
 
     return factors, mean_residual, gamma_prms, n_transitions
 
 
-def _scaling_factors_generation(n_data_type_, days_, save_path, plots):
+def _get_month_factors_gen(n_data_type_, days_, month):
+    f_prev_gen, f_next_gen = [], []
+    for i in range(n_data_type_ - 1):
+        day, next_day = [days_[i_] for i_ in [i, i + 1]]
+        same_id = day["id"] == next_day["id"]
+        subsequent_days = day["cum_day"] + 1 == next_day["cum_day"]
+        current_month = day["month"] == month
+        if (
+                same_id
+                and subsequent_days
+                and current_month
+        ):  # record transition
+            f_prev_gen.append(day["factor"])
+            f_next_gen.append(next_day["factor"])
+
+    return f_prev_gen, f_next_gen
+
+
+def _scaling_factors_generation(n_data_type_, days_, prm):
     # generation
     # obtain subsequent factors
     factors = []
     mean_residual, gamma_prms = {}, {}
     n_transitions = 0
     for i_month, month in enumerate(range(1, 13)):
-        f_prev_gen, f_next_gen = [], []
-        count = 0
-        for i in range(n_data_type_ - 1):
-            day, next_day = [days_[i_] for i_ in [i, i + 1]]
-            same_id = day["id"] == next_day["id"]
-            subsequent_days = day["cum_day"] + 1 == next_day["cum_day"]
-            current_month = day["month"] == month
-            if (
-                    same_id
-                    and subsequent_days
-                    and current_month
-            ):  # record transition
-                count += 1
-                f_prev_gen.append(day["factor"])
-                f_next_gen.append(next_day["factor"])
+        f_prev_gen, f_next_gen \
+            = _get_month_factors_gen(n_data_type_, days_, month)
 
         n_transitions += len(f_prev_gen)
 
         factors.append(
-            _get_correlation(f_prev_gen, f_next_gen, save_path, "gen", plots, month)
+            _get_correlation(
+                f_prev_gen, f_next_gen, prm, "gen", month
+            )
         )
 
         # fit gamma
         if len(f_prev_gen) > 0:
             mean_residual[i_month], gamma_prms[i_month] = _fit_gamma(
                 factors[i_month]["f_prevs"], factors[i_month]["f_nexts"],
-                save_path, "gen", plots, month)
+                prm, "gen", month)
         else:
             mean_residual[i_month], gamma_prms[i_month] = None, None
 
     return factors, mean_residual, gamma_prms, n_transitions
 
 
-def scaling_factors(prm, banks, days, n_data_type, save_path):
-    """
-    Obtain scaling factors corresponding to the data.
-
-    n_data_type: len of bank per data type
-    """
+def _get_factors_stats(prm, days, banks):
     # Initialise dictionaries
     f_max, f_min, f_mean \
         = [initialise_dict(prm["data_types"]) for _ in range(3)]
@@ -358,32 +386,49 @@ def scaling_factors(prm, banks, days, n_data_type, save_path):
         f_min[data_type] = np.min(list_factors)
         f_mean[data_type] = np.mean(list_factors)
 
-    factors, mean_residual, gamma_prms, n_transitions \
-        = _scaling_factors_behaviour_types(prm, banks, save_path)
-
-    if "dem" in prm["data_types"]:
-        for transition in prm["day_trans"]:
-            if gamma_prms['dem'][transition] is None:
-                print(f"gamma_prms['dem'][{transition}] is None")
-
-    if "gen" in prm["data_types"]:
-        factors["gen"], mean_residual["gen"], gamma_prms["gen"], n_transitions["gen"]  \
-            = _scaling_factors_generation(
-            n_data_type["gen"], days["gen"], save_path, prm["plots"])
-
-    folder_path = save_path / "factors"
-    for property_, obj in zip(["f_min", "f_max", "f_mean", "n_transitions", "factors"],
-                              [f_min, f_max, f_mean, n_transitions, factors]):
+    folder_path = prm["save_path"] / "factors"
+    for property_, obj in zip(
+            ["f_min", "f_max", "f_mean"],
+            [f_min, f_max, f_mean]
+    ):
         path = folder_path / f"{property_}.pickle"
         with open(path, "wb") as file:
             pickle.dump(obj, file)
 
-    n_gamma = sum(prm["data_type_source"][data_type] == 'CLNR' for data_type in prm["data_types"])
-    if len(mean_residual.keys()) < n_gamma:
-        print(f"missing mean_residual, mean_residual.keys() = {mean_residual.keys()}")
-    if len(gamma_prms.keys()) < n_gamma:
-        print(f"missing gamma_prms, gamma_prms.keys() = {gamma_prms.keys()}")
+    return f_max, f_min, f_mean
 
-    for property_, obj in zip(["mean_residual", "gamma_prms"], [mean_residual, gamma_prms]):
+
+def scaling_factors(prm, banks, days, n_data_type):
+    """
+    Obtain scaling factors corresponding to the data.
+
+    n_data_type: len of bank per data type
+    """
+    _get_factors_stats(prm, days, banks)
+
+    factors, mean_residual, gamma_prms, n_transitions \
+        = _scaling_factors_behaviour_types(prm, banks)
+
+    if "gen" in prm["data_types"]:
+        [
+            factors["gen"], mean_residual["gen"],
+            gamma_prms["gen"], n_transitions["gen"]
+        ] = _scaling_factors_generation(
+            n_data_type["gen"], days["gen"], prm
+        )
+
+    folder_path = prm["save_path"] / "factors"
+    for property_, obj in zip(
+            ["n_transitions", "factors"],
+            [n_transitions, factors]
+    ):
+        path = folder_path / f"{property_}.pickle"
+        with open(path, "wb") as file:
+            pickle.dump(obj, file)
+
+    for property_, obj in zip(
+            ["mean_residual", "gamma_prms"],
+            [mean_residual, gamma_prms]
+    ):
         with open(folder_path / f"{property_}.pickle", "wb") as file:
             pickle.dump(obj, file)
