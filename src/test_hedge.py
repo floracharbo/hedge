39843aa6b6addbe58c579e25eb54971f55ae426b
floracharbo <flora.charbonnier@live.fr>
2023-04-17 13:36:50 +0100

    Create test_hedge.py

---
+++ src/test_hedge.py
@@ -0,0 +1,10 @@
+from src.hedge import HEDGE
+
+generator = HEDGE(
+    n_homes=10,
+    factors_gen='matrix',
+    n_consecutive_days=2,
+    brackets_definition='percentile'
+)
+for i in range(10):
+    generator.make_next_day(plotting=True)
