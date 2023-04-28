cdfa2a8ad5b6ea9fd56b9a389cc50737bb41ed28
floracharbo <flora.charbonnier@live.fr>
2023-04-28 10:05:53 +0100

    import save_fig from utils

--- src/data_preparation/define_blocks.py
+++ src/data_preparation/define_blocks.py
@@ -14,7 +14,8 @@ import numpy as np
 import seaborn as sns
 
 from src.data_preparation.filling_in import stats_filling_in
-from src.utils import data_id, initialise_dict, list_potential_paths_outs, save_fig
+from src.utils import (data_id, initialise_dict, list_potential_paths_outs,
+                       save_fig)
 
 
 def _load_out(prm, data_type, chunk_rows):
