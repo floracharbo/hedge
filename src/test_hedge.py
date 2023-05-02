3881289331e2103d752c9988b6de6e07cc82b32f
floracharbo <flora.charbonnier@live.fr>
2023-05-02 20:08:18 +0100

    remove mention of matrix vs correlation

--- src/test_hedge.py
+++ src/test_hedge.py
@@ -2,9 +2,8 @@ from src.hedge import HEDGE
 
 generator = HEDGE(
     n_homes=10,
-    factors_gen='matrix',
     n_consecutive_days=2,
-    brackets_definition='percentile'
+    brackets_definition='percentile',
 )
 
 for i in range(10):
