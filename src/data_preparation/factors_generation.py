f6c64dc683fdbde30e99e271b55e66c41424b143
floracharbo <flora.charbonnier@live.fr>
2023-05-02 10:00:34 +0100

    200 epochs

--- src/data_preparation/factors_generation.py
+++ src/data_preparation/factors_generation.py
@@ -600,7 +600,7 @@ def compute_profile_generators(
     params = {
         'profiles': True,
         'batch_size': 100,
-        'n_epochs': 2,
+        'n_epochs': 200,
         # 'lr_start': 0.1,
         # 'lr_end': 0.01,
         # 'weight_sum_profiles': 1e-3 * 10 * 10,
