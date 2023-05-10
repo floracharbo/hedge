332a23c6374e7b3fe3e6b3fe59aacb9386470951
floracharbo <flora.charbonnier@live.fr>
2023-05-10 14:03:08 +0100

    debug vmin problem

--- src/data_preparation/scaling_factors.py
+++ src/data_preparation/scaling_factors.py
@@ -321,7 +321,10 @@ def _transition_intervals(
         if prm["plots"] and n_consecutive_days == 2:
             fig, ax = plt.subplots()
             min_trans_prob = np.min(trans_prob[trans_prob > 0]) if np.sum(trans_prob > 0) > 0 else 10
+            print(f"min_trans_prob {min_trans_prob}")
             vmin = np.min([1e-4, min_trans_prob])
+            print(f"vmin {vmin}")
+            np.save('trans_prob', trans_prob)
             cmap = get_cmap()
             cmap.set_bad(color='white')
             img = ax.imshow(trans_prob, norm=LogNorm(vmin=vmin), origin='lower', cmap=cmap)
