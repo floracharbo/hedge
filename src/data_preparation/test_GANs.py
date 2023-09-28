c5f5e79342a50579699a9b7b246a17df1d0ae879
floracharbo <flora.charbonnier@live.fr>
2023-09-28 18:23:10 +0100

    Create test_GANs.py

---
+++ src/data_preparation/test_GANs.py
@@ -0,0 +1,110 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
+import torch as th
+
+
+def shuffle(X, y):
+    num_samples = len(X)
+    random_indexes = np.random.permutation(num_samples)
+    X = X[random_indexes]
+    y = y[random_indexes]
+
+    return X, y
+
+
+def generate_synthetic_data(n_train_data, generators, ks):
+    profiles = {}
+    for k in ks:
+        n_batch = int(n_train_data[k] / 50) + 1
+        profiles[k] = generators[k](th.randn(n_batch, 1)).view(-1, 24).detach().numpy()[:n_train_data[k]]
+
+    return profiles
+
+def predict_clusters(generators, vals_k, n_train_data, n_test_data, n_repeats):
+    setups = {
+        'baseline': {
+            'train_data': 'real_train',
+            'test_data': 'real_test',
+        },
+        'TSTR': {
+            'train_data': 'synthetic',
+            'test_data': 'real_test'
+        },
+        'TRTS': {
+            'train_data': 'real_test',
+            'test_data': 'synthetic',
+        },
+    }
+    for setup_label, setup in setups.items():
+        print(f"Setup: {setup_label}")
+        scores = []
+        for repeat in range(n_repeats):
+            profiles_train = {}
+            profiles_test = {}
+            if setup['train_data'] == 'real_train':
+                for k in vals_k:
+                    profiles_train[k] = vals_k[k][0: n_train_data[k]]
+            elif setup['train_data'] == 'real_test':
+                for k in vals_k:
+                    profiles_train[k] = vals_k[k][n_train_data[k]:]
+            elif setup['train_data'] == 'synthetic':
+                profiles_train = generate_synthetic_data(n_train_data, generators, list(vals_k.keys()))
+
+            X_train = np.concatenate([profiles_train[k] for k in vals_k])
+            y_train = np.concatenate([np.full(len(profiles_train[k]), k) for k in profiles_train])
+
+            if setup['test_data'] == 'real_test':
+                for k in vals_k:
+                    profiles_test[k] = vals_k[k][n_train_data[k]:]
+            elif setup['test_data'] == 'synthetic':
+                profiles_test = generate_synthetic_data(n_test_data, generators, list(vals_k.keys()))
+
+            X_test = np.concatenate([profiles_test[k] for k in vals_k])
+            y_test = np.concatenate([np.full(len(profiles_test[k]), k) for k in profiles_train])
+
+            # Shuffle the data
+            X_train, y_train = shuffle(X_train, y_train)
+            X_test, y_test = shuffle(X_test, y_test)
+
+            # Train the classifier
+            classifier = RandomForestClassifier()
+            classifier.fit(X_train, y_train)
+            y_pred = classifier.predict(X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+            scores.append(accuracy)
+
+        print(f"Average accuracy {setup_label} over {n_repeats}: {np.mean(scores)}")
+
+
+def discriminate_real_synthetic(vals_k, generators, n_train_data, n_test_data, n_repeats):
+    for k in vals_k:
+        scores = []
+        for repeat in range(n_repeats):
+            synthetic_data_train = generate_synthetic_data(n_train_data, generators, [k])[k]
+            X_train = np.concatenate([vals_k[k][0: n_train_data[k]], synthetic_data_train])
+            y_train = np.concatenate([np.full(n_train_data[k], 1), np.full(n_train_data[k], 0)])
+            X_train, y_train = shuffle(X_train, y_train)
+
+            synthetic_data_test = generate_synthetic_data(n_test_data, generators, [k])[k]
+            X_test = np.concatenate([vals_k[k][n_train_data[k]:], synthetic_data_test])
+            y_test = np.concatenate([np.full(n_test_data[k], 1), np.full(n_test_data[k], 0)])
+            X_test, y_test = shuffle(X_test, y_test)
+
+            classifier = RandomForestClassifier()
+            classifier.fit(X_train, y_train)
+            y_pred = classifier.predict(X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+            scores.append(accuracy)
+        print(
+            f"Average accuracy discriminating synthetic profiles in cluster {k}"
+            f"over {n_repeats} repeats: {np.mean(scores)}"
+        )
+
+def test_GANs(generators, vals_k, train_set_size):
+    n_repeats = 10
+
+    n_train_data = {k: int(len(vals_k[k]) * train_set_size) for k in vals_k}
+    n_test_data = {k: len(vals_k[k]) - n_train_data[k] for k in vals_k}
+    predict_clusters(generators, vals_k, n_train_data, n_test_data, n_repeats)
+    discriminate_real_synthetic(vals_k, generators, n_train_data, n_test_data, n_repeats)
